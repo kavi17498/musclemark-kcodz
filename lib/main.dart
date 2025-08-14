2da927441b48fddc70c885260485558ddfac5103
Kappanile Kavindu Lakshan Kottegoda <138689980+kavi17498@users.noreply.github.com>
2025-08-14 13:51:31 +0530

    hive registerd

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:musclemark/models/workout.dart';
 import 'package:musclemark/routes/app_router.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:hive/hive.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocDir = await getApplicationDocumentsDirectory();
+
+  Hive.init(appDocDir.path);
+  Hive.registerAdapter(WorkoutAdapter());
+
+  await Hive.openBox<Workout>('workouts');
+
   runApp(const MyApp());
 }
 
